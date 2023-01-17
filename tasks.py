b706bdf4a1f39007320f749ae98ec30ecb6f7535
Matthew Evans <git@ml-evs.science>
2023-01-17 15:45:21 +0000

    Add task for validating entries, move example data

--- tasks.py
+++ tasks.py
@@ -13,6 +13,8 @@ def regenerate_models(_):
 
     schemas = glob.glob("./schemas/schemas/*.yml")
 
+    print("Regenerating pydantic models")
+
     if not schemas:
         raise RuntimeError("No schemas found")
 
@@ -24,4 +26,24 @@ def regenerate_models(_):
         with open(MODEL_DIRECTORY / f"{schema_path.name.strip('.yml')}.py", "w") as f:
             f.writelines(output)
 
-        print("Done")
+    print("Done!")
+
+
+@task(pre=[regenerate_models])
+def validate_entries(_):
+
+    print("Validating entries")
+
+    from marda_registry.models import Extractor, FileType
+    from marda_registry.utils import load_registry_collection
+
+    counts = {}
+    for type_ in (FileType, Extractor):
+        counts[type_] = load_registry_collection(
+            type_,
+            database=None,
+            validate=True,
+        )
+        print(f"Loaded {counts[type_]} {type_.__name__} entries")
+
+    print("Done!")
