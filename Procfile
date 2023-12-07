b8bd05d19ab0092fb2531654c010239c776f0395
Matthew Evans <git@ml-evs.science>
2023-12-07 22:22:54 +0000

    Bump API version and revert Procfile change

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: uvicorn marda_registry.api:api --host 0.0.0.0 --port $PORT
+web: uvicorn marda_registry.app:app --host 0.0.0.0 --port $PORT
--- marda_registry/app.py
+++ marda_registry/app.py
@@ -15,7 +15,7 @@ from pydantic import BaseModel
 from .models import Extractor, FileType
 from .utils import load_registry_collection
 
-__api_version__ = "0.2.0"
+__api_version__ = "0.3.0"
 
 
 app = FastAPI(
